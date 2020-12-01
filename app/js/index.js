b885094b30874a733cfe3a0e24726e5caaf8e698
Felix Freidman <felixfreidman@gmail.com>
2020-12-02 00:45:46 +0800

    Select
    Просто обосраться, я смог сделать свой select, я просто охуел

--- app/js/index.js
+++ app/js/index.js
@@ -1,7 +1,14 @@
 let realEstateTypeField = document.getElementById("realEstateTypeField");
 let realEstateRadioArray = document.querySelectorAll("input[name=realEstateType]");
+let realEstateTypeLabel = document.getElementById("realEstateTypeLabel");
 let roomAmountField = document.getElementById("roomAmountField");
 let roomAmountCheckboxArray = document.querySelectorAll("input[name=roomAmount]");
+let roomAmountLabel =  document.getElementById("roomAmountLabel");
+let chosenCityField = document.getElementById("chosenCityField");
+let chosenCityRadioArray = document.querySelectorAll("input[name=chosenCity]");
+let chosenCityLabel = document.getElementById("chosenCityLabel");
+
+
 
 realEstateRadioArray.forEach(radio => {
     if(radio.checked) {
@@ -10,6 +17,7 @@ realEstateRadioArray.forEach(radio => {
     radio.addEventListener("click", () => {
         if(radio.checked) {
             realEstateTypeField.innerHTML = `${radio.value}`;
+            realEstateTypeLabel.checked = false;
         }
     });
 });
@@ -18,13 +26,19 @@ realEstateRadioArray.forEach(radio => {
 roomAmountCheckboxArray.forEach(checkbox => {
     if(checkbox.checked) {
         roomAmountField.innerHTML = `${checkbox.value}`;
-        console.log(roomAmountField.innerHTML);
     }
     checkbox.addEventListener("click", () => {
             roomAmountField.innerHTML = `${checkbox.value}`;
-            console.log(roomAmountField.innerHTML);
+            roomAmountLabel.checked = false;
     });
 });
 
-console.log(roomAmountCheckboxArray);
-console.log(roomAmountField);
+chosenCityRadioArray.forEach(radio => {
+    if(radio.checked) {
+        chosenCityField.innerHTML = `${radio.value}`;
+    }
+    radio.addEventListener("click", () => {
+        chosenCityField.innerHTML = `${radio.value}`;
+        chosenCityLabel.checked = false;
+    });
+});
