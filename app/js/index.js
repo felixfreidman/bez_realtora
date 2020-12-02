f96db0700c7c5dc35a3cef12146b0f45d4abe538
Felix Freidman <felixfreidman@gmail.com>
2020-12-02 11:51:06 +0800

    11:53 Commit
     Закончил верстку секции быстрого поиска, начал делать секцию истории

--- app/js/index.js
+++ app/js/index.js
@@ -1,17 +1,16 @@
 let realEstateTypeField = document.getElementById("realEstateTypeField");
-let realEstateRadioArray = document.querySelectorAll(
-  "input[name=realEstateType]"
-);
+let realEstateRadioArray = document.querySelectorAll("input[name=realEstateType]");
 let realEstateTypeLabel = document.getElementById("realEstateTypeLabel");
 let roomAmountField = document.getElementById("roomAmountField");
-let roomAmountCheckboxArray = document.querySelectorAll(
-  "input[name=roomAmount]"
-);
+let roomAmountCheckboxArray = document.querySelectorAll("input[name=roomAmount]");
 let roomAmountLabel = document.getElementById("roomAmountLabel");
 let chosenCityField = document.getElementById("chosenCityField");
 let chosenCityRadioArray = document.querySelectorAll("input[name=chosenCity]");
 let chosenCityLabel = document.getElementById("chosenCityLabel");
 let priceRangeField = document.getElementById("priceRangeField");
+
+// Сначала значение label становится отмеченным по умолчанию, затем на каждое радио мы ждем, что если 
+// оно отмеченно, то значение label становится как у отмеченного радио
 
 realEstateRadioArray.forEach((radio) => {
   if (radio.checked) {
@@ -26,25 +25,6 @@ realEstateRadioArray.forEach((radio) => {
   });
 });
 
-realEstateTypeField.addEventListener("click",()=> {
-  realEstateTypeField.classList.toggle("reversed-arrow");
-});
-
-roomAmountCheckboxArray.forEach((checkbox) => {
-  if (checkbox.checked) {
-    roomAmountField.innerHTML = `${checkbox.value}`;
-  }
-  checkbox.addEventListener("click", () => {
-    roomAmountField.innerHTML = `${checkbox.value}`;
-    // roomAmountLabel.checked = false;
-  });
-});
-
-
-roomAmountField.addEventListener("click",()=> {
-  roomAmountField.classList.toggle("reversed-arrow");
-});
-
 chosenCityRadioArray.forEach((radio) => {
   if (radio.checked) {
     chosenCityField.innerHTML = `${radio.value}`;
@@ -55,14 +35,23 @@ chosenCityRadioArray.forEach((radio) => {
     chosenCityField.classList.toggle("reversed-arrow");
   });
 });
-chosenCityField.addEventListener("click",()=> {
+
+realEstateTypeField.addEventListener("click", () => {
+  realEstateTypeField.classList.toggle("reversed-arrow");
+});
+
+roomAmountField.addEventListener("click", () => {
+  roomAmountField.classList.toggle("reversed-arrow");
+});
+
+chosenCityField.addEventListener("click", () => {
   chosenCityField.classList.toggle("reversed-arrow");
 });
 
-priceRangeField.addEventListener("click",()=> {
+priceRangeField.addEventListener("click", () => {
   priceRangeField.classList.toggle("reversed-arrow");
 });
-
+// Рабочий сладйер, реализация через widget
 $(function () {
   $arrayOfValues = [3000, 205000];
   $("#slider-range").slider({
@@ -70,16 +59,14 @@ $(function () {
     step: 100,
     min: 0,
     max: 205000,
-    values: [15000,18000],
+    values: [15000, 18000],
     slide: function (event, ui) {
       $("#amount").val(ui.values[0] + "₽ - " + ui.values[1] + "₽");
-      $("#min-value").val(ui.values[0] );
+      $("#min-value").val(ui.values[0]);
       $("#max-value").val(ui.values[1]);
-      
     },
-   
   });
-  
+
   $("#amount").val(
     $("#slider-range").slider("values", 0) +
       "₽ - " +
@@ -89,24 +76,21 @@ $(function () {
 
   $("#min-value").val($("#slider-range").slider("values", 0));
   $("#max-value").val($("#slider-range").slider("values", 1));
-  
 });
 
-$('#min-value').on('input',function(){
+// Сам писал, чтобы конечная цена менялась по ручному изменению input-a
+$("#min-value").on("input", function () {
   $minimalValue = $("#min-value").val();
   $maxValue = $("#max-value").val();
-  if($minimalValue > 0 && $minimalValue < 205000) {
-  $("#amount").val($minimalValue + "₽ - " + $maxValue + "₽");
+  if ($minimalValue > 0 && $minimalValue < 205000) {
+    $("#amount").val($minimalValue + "₽ - " + $maxValue + "₽");
   }
 });
 
-$('#max-value').on('input',function(){
+$("#max-value").on("input", function () {
   $minimalValue = $("#min-value").val();
   $maxValue = $("#max-value").val();
-  if($maxValue > 0 && $maxValue < 205000) {
-  $("#amount").val($minimalValue + "₽ - " + $maxValue + "₽");
+  if ($maxValue > 0 && $maxValue < 205000) {
+    $("#amount").val($minimalValue + "₽ - " + $maxValue + "₽");
   }
 });
-
-
-
