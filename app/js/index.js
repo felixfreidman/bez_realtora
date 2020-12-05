a68c0e7b02a3a6aa6ec8f3d9e8f213a2fe5614cb
Felix Freidman <felixfreidman@gmail.com>
2020-12-06 04:18:03 +0800

    4:17PM Commit
    Доделал секцию преимущества и отзывы, Сука нахуй гавно делаю

--- app/js/index.js
+++ app/js/index.js
@@ -94,3 +94,16 @@ $("#max-value").on("input", function () {
     $("#amount").val($minimalValue + "₽ - " + $maxValue + "₽");
   }
 });
+
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
