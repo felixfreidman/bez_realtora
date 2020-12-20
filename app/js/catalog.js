7329fb5280f7cec4e5de45d6415df1de4a942cbd
Felix Freidman <felixfreidman@gmail.com>
2020-12-20 09:47:55 +0800

    9:47AM Commit
    Доделал полносью секцию фильтры
    Осталось доделать секцию фильтры

--- app/js/catalog.js
+++ app/js/catalog.js
@@ -573,68 +573,6 @@ moreButton.addEventListener("click", () => {
                 >
               </div>
             </div>
-            <div class="advertisments-section_advert-card">
-              <div class="advert-card_panel-info-section">
-                <div class="advert-card_info-section">
-                  <div class="info-section_avatar"></div>
-                  <span class="info-section_name">Александра Соколовская</span>
-                </div>
-              </div>
-              <div class="advert-card_objPhotos-section">
-                <img
-                  src="../img/main/main_rentAdvertisment-card.jpg"
-                  alt=":("
-                />
-                <img
-                  src="../img/main/main_rentAdvertisment-card.jpg"
-                  alt=":("
-                />
-                <img
-                  src="../img/main/main_rentAdvertisment-card.jpg"
-                  alt=":("
-                />
-                <img
-                  src="../img/main/main_rentAdvertisment-card.jpg"
-                  alt=":("
-                />
-                <img
-                  src="../img/main/main_rentAdvertisment-card.jpg"
-                  alt=":("
-                />
-              </div>
-              <div class="advert-card_panel-section">
-                <div class="panel-section_section unfilledHeart"></div>
-                <div class="panel-section_section counter">999</div>
-                <div class="panel-section_section unfilledShare"></div>
-              </div>
-              <div class="advert-card_objInfo-section">
-                <div class="objInfo-section_price-tag">
-                  <span class="price-tag_price">155 000 ₽</span>
-                  <span class="price-tag_per-month">/в месяц</span>
-                </div>
-                <div class="objInfo-section_flat-parameters">
-                  <span class="flat-parameters_roomAmount-span">4-комн.</span>
-                  -
-                  <span class="flat-parameters_flatArea-span">170 м²</span>
-                  -
-                  <span class="flat-parameters_flatAddress-span"
-                    >Франк-Каменецкого, 28/4</span
-                  >
-                </div>
-                <div class="objInfo-section_flat-description">
-                  Роскошная 4х комнатная квартира
-                </div>
-              </div>
-              <div class="advert-card_objAddress-section">
-                <img
-                  src="../img/main/main_map-checkmark.svg"
-                  class="objAddress-section_map-checkmark"
-                />
-                <span class="objAddress-section_address-span"
-                  >Правобережный</span
-                >
-              </div>
-            </div>
           </div>
           <div class="main_applicationForm-section">
             <div class="applicationForm">
@@ -676,68 +614,6 @@ moreButton.addEventListener("click", () => {
             </div>
           </div>
           <div class="advertisment-section_part">
-            <div class="advertisments-section_advert-card">
-              <div class="advert-card_panel-info-section">
-                <div class="advert-card_info-section">
-                  <div class="info-section_avatar"></div>
-                  <span class="info-section_name">Александра Соколовская</span>
-                </div>
-              </div>
-              <div class="advert-card_objPhotos-section">
-                <img
-                  src="../img/main/main_rentAdvertisment-card.jpg"
-                  alt=":("
-                />
-                <img
-                  src="../img/main/main_rentAdvertisment-card.jpg"
-                  alt=":("
-                />
-                <img
-                  src="../img/main/main_rentAdvertisment-card.jpg"
-                  alt=":("
-                />
-                <img
-                  src="../img/main/main_rentAdvertisment-card.jpg"
-                  alt=":("
-                />
-                <img
-                  src="../img/main/main_rentAdvertisment-card.jpg"
-                  alt=":("
-                />
-              </div>
-              <div class="advert-card_panel-section">
-                <div class="panel-section_section unfilledHeart"></div>
-                <div class="panel-section_section counter">999</div>
-                <div class="panel-section_section unfilledShare"></div>
-              </div>
-              <div class="advert-card_objInfo-section">
-                <div class="objInfo-section_price-tag">
-                  <span class="price-tag_price">155 000 ₽</span>
-                  <span class="price-tag_per-month">/в месяц</span>
-                </div>
-                <div class="objInfo-section_flat-parameters">
-                  <span class="flat-parameters_roomAmount-span">4-комн.</span>
-                  -
-                  <span class="flat-parameters_flatArea-span">170 м²</span>
-                  -
-                  <span class="flat-parameters_flatAddress-span"
-                    >Франк-Каменецкого, 28/4</span
-                  >
-                </div>
-                <div class="objInfo-section_flat-description">
-                  Роскошная 4х комнатная квартира
-                </div>
-              </div>
-              <div class="advert-card_objAddress-section">
-                <img
-                  src="../img/main/main_map-checkmark.svg"
-                  class="objAddress-section_map-checkmark"
-                />
-                <span class="objAddress-section_address-span"
-                  >Правобережный</span
-                >
-              </div>
-            </div>
             <div class="advertisments-section_advert-card">
               <div class="advert-card_panel-info-section">
                 <div class="advert-card_info-section">
